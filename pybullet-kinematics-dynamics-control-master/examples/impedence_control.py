9dbed758913b6b856fbb860207669b453ecfeeb7
AvidLuv <1764741747@qq.com>
2025-07-13 14:23:33 +0100

    Initial push

--- pybullet-kinematics-dynamics-control-master/examples/impedence_control.py
+++ pybullet-kinematics-dynamics-control-master/examples/impedence_control.py
@@ -10,7 +10,7 @@ robot.createWorld(GUI=True)
 thi = np.array([0, -1.0, 1.0, -1.57, -1.57, -1.57, 0]) # initial joint angles
 thf = np.array([-1.5, -1.0, 1.0, -1.57, -1.57, -1.57, 0]) # final joint nagles
 robot.setJointPosition(thi)
-desired_pose = np.array([-0.10857937593446423, 0.6166151451748437, 0.667087828094798, -1.5700006464761673, 0.0007970376813502642, 1.5692036732274044]) #ur5-link
+desired_pose = np.array([-0.10857937593446423, 0.6166151451748437, 0.667087828094798, -1.5700006464761673, 0.0007970376813502642, 1.5692036732274044])
 robot.impedenceController_null(thi, desired_pose, controller_gain=30)
 
 
